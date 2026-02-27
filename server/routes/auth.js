1a30b23fee83fed00f0441ea90e74b03b8b2b0a5
Devon Kay <dev.m.kay@gmail.com>
2026-02-26 18:39:51 -0600

    moved code to authcontroller

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,37 +1,4 @@
 const router = require('express').Router();
-const express = require('express')
-const {createClient} = require('@supabase/supabase-js');
-
-
-const app = express()
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
-
-const supabase = createClient(
-    process.env.SUPABASE_URL,
-    process.env.SUPABASE_ANON_KEY
-);
-
-app.listen(3000, () => {
-    console.log('Server running on port 3000');
-});
-
-app.get('/register', (req, res)=>{
-    //return signup.html
-});
-
-app.post('/register', async(req, res) =>{
-    const {email, password} = req.body;
-    const {data, error} = await supabase.auth.signUp({
-        email,
-        password
-    });
-    if (error){
-        return res.status(400).json({error: error.message});
-    }
-
-    return res.status(200).json({message: 'User created successfully'});
-});
 
 // TODO: Add auth routes (POST /register, POST /login, GET /me)
 
