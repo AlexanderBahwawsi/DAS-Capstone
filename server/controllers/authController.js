23791cf0805ebbc19a3741db99e8b6d39215d61d
Devon Kay <dev.m.kay@gmail.com>
2026-03-12 18:15:35 -0500

    api/auth/me

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -75,11 +75,11 @@ app.post('api/auth/register', async(req, res) =>{
 app.get('api/auth/me', async(req, res) => {
     try {
     // Fetch user details using decoded token
-    const user = await User.findById({ email: req.user.email });
+    const user = await User.findById({ email: req.user.id });
     if (!user) {
       return res.status(404).json({ error: 'User not found' });
     }
-    res.status(200).json({ username: user.username, email: user.email });
+    res.status(200).json({ id: user.id, email: user.email });
   } catch (error) {
     res.status(500).json({ error: 'Internal server error' });
   }
