f2676607c5afb087dc8778332e6507748069809c
AlexanderBahwawsi <132516699+AlexanderBahwawsi@users.noreply.github.com>
2026-03-25 13:25:10 -0500

    Created admin routes

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -1,5 +1,20 @@
 const router = require('express').Router();
+const ctrl = require('../controllers/adminController');
+const { authenticate } = require('../middleware/auth');
+const { authorize } = require('../middleware/roles');
 
-// TODO: Add admin routes (users, assignments, bulk status, export)
+router.use(authenticate);
+router.use(authorize('admin'));
+
+router.get('/users',                                    ctrl.getAllUsers);
+router.put('/users/:id/role',                           ctrl.updateUserRole);
+router.delete('/users/:id',                             ctrl.deleteUser);
+
+router.post('/assign',                                  ctrl.assignReviewer);
+router.delete('/assign/:submissionId/:reviewerId',      ctrl.removeAssignment);
+router.get('/workload',                                 ctrl.getReviewerWorkload);
+
+router.put('/submissions/bulk-status',                  ctrl.bulkUpdateStatus);
+router.get('/export',                                   ctrl.exportData);
 
 module.exports = router;
